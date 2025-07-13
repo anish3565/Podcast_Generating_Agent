c3329c8e998c4296aada48963aa3e4d64e5764af
anish3565 <tripathianish12@gmail.com>
2025-07-13 15:27:46 +0530

    Command_List file updated

--- command_list.py
+++ command_list.py
@@ -1,3 +1,5 @@
 Bash
 1. docker build -t blog-summarizer-img .
 2. docker run -it --env-file .env blog-summarizer-img
+3. docker build -t final-podcast-img .
+4. docker run -it --env-file .env final-podcast-img
